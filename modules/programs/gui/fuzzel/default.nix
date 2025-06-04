9e533b558f052b338483be2d910b3e331740fce5
ag1O1 <amrashraf20210@gmail.com>
2025-06-04 11:02:20 +0300

    update fuzzel to use hjem-rum

--- modules/programs/gui/fuzzel/default.nix
+++ modules/programs/gui/fuzzel/default.nix
@@ -4,40 +4,37 @@
   lib,
   ...
 }:
-with lib; let
+with lib;
+let
   cfg = config.modules.programs.gui.fuzzel;
   inherit (config.theme) fonts colors;
-  fuzzle-conf = ''
-    dpi-aware=no
-    icon-theme=Papirus-Dark
-    width=25
-    font=${fonts.sans.name}weight:bold:size=36
-    line-height=50
-    fields=name,generic,comment,categories,filename,keywords
-    terminal=foot -e
-    prompt="❯   "
-    show-actions=yes
-    exit-on-keyboard-focus-loss=no
-    [colors]
-    background=${colors.base00}
-    selection=${colors.base0B}
-    border=${colors.base0B}
-
-    [border]
-    radius=20
-
-    [dmenu]
-    exit-immediately-if-empty=yes
-  '';
-in {
+in
+{
   options.modules.programs.gui.fuzzel = {
     enable = lib.mkEnableOption "fuzzel";
   };
   config = mkIf cfg.enable {
-    hj = {
-      packages = [pkgs.fuzzel];
-      files = {
-        "~/.config/fuzzel/fuzzel.ini".text = fuzzle-conf;
+    hj.rum.programs.fuzzel = {
+      enable = true;
+      package = pkgs.fuzzel;
+      settings = {
+        main = {
+          terminal = "foot -e";
+          icon-theme = "Papirus-Dark";
+          dpi-aware = "no";
+          width = "20";
+          font = "${fonts.sans.name}weight:bold:size=15";
+          line-height = "25";
+          fields = "name,generic,comment,categories,filename,keywords";
+          prompt = "❯   ";
+          show-actions = "yes";
+          exit-on-keyboard-focus-loss = "no";
+        };
+        colors = {
+          background = "${colors.base00}ff";
+          selection = "${colors.base0B}ff";
+          border = "${colors.base0B}ff";
+        };
       };
     };
   };
