25a72fe8c8ed4d0b509966950cc537dbc83a5713
ag1O1 <amrashraf20210@gmail.com>
2025-06-04 11:04:45 +0300

    fix qt

--- modules/theming/qt/default.nix
+++ modules/theming/qt/default.nix
@@ -1,15 +1,21 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   qt = {
     enable = true;
-    platformTheme = "qt5ct";
     style = "kvantum";
+    platformTheme = "qt5ct";
   };
-  /*
-  hj.packages = [
-    (pkgs.catppuccin-kde.override {
-      flavour = ["mocha"];
-      accents = ["blue"];
-    })
-  ];
-  */
+  hj = {
+    packages = [
+      (pkgs.catppuccin-kvantum.override {
+        variant = "mocha";
+        accent = "blue";
+      })
+      (pkgs.catppuccin-kde.override {
+        flavour = [ "mocha" ];
+        accents = [ "blue" ];
+      })
+    ];
+
+  };
 }
