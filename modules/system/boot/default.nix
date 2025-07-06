40d96e0437251b508457ec39071f74fbbba976ee
ag1O1 <amrashraf20210@gmail.com>
2025-07-06 17:33:48 +0300

    rever nvidia kernel fix (not needed anymore)

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -7,34 +7,8 @@
 {
   boot = {
     #kernelPackages = pkgs.linuxPackages_xanmod_latest;
-    kernelPackages =
-      let
-        apply = _: prevModules: {
-          v4l2loopback =
-            if lib.strings.hasPrefix "0.13.2" prevModules.v4l2loopback.version then
-              prevModules.v4l2loopback.overrideAttrs (_: rec {
-                version = "0.15.0";
-                src = pkgs.fetchFromGitHub {
-                  owner = "umlaeute";
-                  repo = "v4l2loopback";
-                  rev = "v${version}";
-                  hash = "sha256-fa3f8GDoQTkPppAysrkA7kHuU5z2P2pqI8dKhuKYh04=";
-                };
-              })
-            else
-              prevModules.v4l2loopback;
-        };
-      in
-      pkgs.linuxPackages_cachyos-lto.extend apply;
-    #kernelPackages = pkgs.linux_cachyos;
+    kernelPackages = pkgs.linuxPackages_cachyos-lto;
     kernelParams = [
-      "nopvspin"
-      "skew_tick=1"
-      "rcupdate.rcu_expedited=1"
-      "tsc=nowatchdog"
-      "kvm-amd.nested=0"
-      "kvm-amd.npt=1"
-      "kvm-amd.avic=1"
       "sysrq_always_enabled=0"
       "ftrace_enabled=0"
     ];
