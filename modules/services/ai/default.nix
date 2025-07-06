dad36a6f60b7cd5e4f9d9f573fbe4e545508eda8
ag1O1 <amrashraf20210@gmail.com>
2025-07-06 17:34:51 +0300

    disable ollama (broken again)

--- modules/services/ai/default.nix
+++ modules/services/ai/default.nix
@@ -19,6 +19,13 @@ in
     enable = lib.mkEnableOption "ai";
   };
   config = mkIf cfg.enable {
+
+    # ollama cuda seems to currently be broken, using docker for now
+    virtualisation.docker = {
+      enable = true;
+    };
+    hardware.nvidia-container-toolkit.enable = true;
+    environment.systemPackages = [ pkgs.nvidia-container-toolkit ];
     services = {
       /*
         llama-cpp = {
@@ -26,15 +33,19 @@ in
         openFirewall = true;
         };
       */
-      ollama = {
-        enable = true;
-        acceleration = "cuda";
-        package = nixpkgs-stable.ollama;
-        environmentVariables = {
-          OLLAMA_FLASH_ATTENTION = "1";
-          OLLAMA_KV_CACHE_TYPE = "q4_0";
+      /*
+        ollama = {
+          enable = true;
+          acceleration = false;
+          host = "0.0.0.0";
+          port = 11434;
+          rocmOverrideGfx = "10.3.0";
+          environmentVariables = {
+            OLLAMA_FLASH_ATTENTION = "1";
+            OLLAMA_KV_CACHE_TYPE = "q4_0";
+          };
         };
-      };
+      */
       open-webui = {
         enable = true;
         package = nixpkgs-stable.open-webui;
