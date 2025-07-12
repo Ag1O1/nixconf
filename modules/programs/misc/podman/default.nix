c7dd6dd63c4658e7205fc02f9abc8143c66cef48
ag1O1 <amrashraf20210@gmail.com>
2025-07-12 13:17:23 +0300

    podman

---
+++ modules/programs/misc/podman/default.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.modules.programs.misc.podman;
+in {
+  options.modules.programs.misc.podman = {
+    enable = lib.mkEnableOption "podman";
+  };
+  config = mkIf cfg.enable {
+    virtualisation.podman = {
+      enable = true;
+      dockerCompat = true;
+
+    };
+  };
+}
