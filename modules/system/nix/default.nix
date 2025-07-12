06c3f6e0245a4689087d9d02dce65597c050a798
ag1O1 <amrashraf20210@gmail.com>
2025-07-12 13:38:40 +0300

    fix trusted public keys + cleanup

--- modules/system/nix/default.nix
+++ modules/system/nix/default.nix
@@ -8,18 +8,16 @@
     "dotnet-sdk-6.0.428"
   ];
   nix = {
-    package = pkgs.lix;
     optimise.automatic = true;
 
     settings = {
       substituters = [
-        "https://hyprland.cachix.org"
-        "https://cosmic.cachix.org/"
+        #"https://hyprland.cachix.org"
+        #"https://cosmic.cachix.org"
       ];
       trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-        "niri.cachix.org"
+        #"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        #"cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
       ];
       experimental-features = [
         "nix-command"
