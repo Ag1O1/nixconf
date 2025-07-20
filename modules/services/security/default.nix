4fceae221689e532c60d5993428707b708af6471
ag1O1 <amrashraf20210@gmail.com>
2025-07-20 10:51:11 +0300

    add some security options

---
+++ modules/services/security/default.nix
@@ -0,0 +1,27 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+with lib;
+let
+  cfg = config.modules.services.security;
+in
+{
+  options.modules.services.security = {
+    enable = lib.mkEnableOption "Security";
+  };
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+
+    ];
+    services = {
+      clamav = {
+        daemon.enable = true;
+        scanner.enable = true;
+      };
+      fail2ban.enable = true;
+    };
+  };
+}
