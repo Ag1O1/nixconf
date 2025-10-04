ad9d26babdecbec829e55e3c5be3d8868f87ba7e
ag1O1 <amrashraf20210@gmail.com>
2025-10-04 19:08:23 +0300

    add host: Lap

---
+++ hosts/Lap/configuration.nix
@@ -0,0 +1,145 @@
+# main configuration
+# this should remain short and only to set and enable settings from custom modules
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  user = "amr";
+in {
+  imports = [
+    ./hardware-configuration.nix
+    #./hardware
+    ./packages.nix
+    (
+      lib.mkAliasOptionModule
+      ["hj"]
+      [
+        "hjem"
+        "users"
+        "${user}"
+      ]
+    )
+  ];
+  #hjem stuff
+  hjem = {
+    users.amr = {
+      enable = true;
+      directory = "/home/amr";
+      user = "amr";
+    };
+    clobberByDefault = true;
+    extraModules = [
+      inputs.hjem-rum.hjemModules.default
+    ];
+  };
+  # hostname
+  networking.hostName = "nixos";
+  #networking.useDHCP = lib.mkDefault true;
+
+  # user
+  time.timeZone = "Africa/Cairo";
+  i18n.defaultLocale = "en_US.UTF-8";
+  users.users = {
+    amr = {
+      initialPassword = "password";
+      shell = pkgs.fish;
+      isNormalUser = true;
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "libvirtd"
+        "scanner"
+        "lp"
+        "video"
+        "kvm"
+        "libvirt"
+        "docker"
+      ];
+    };
+  };
+
+  # module configurations
+
+  modules = {
+    programs = {
+      gui = {
+        #firefox.enable = true;
+        discord.enable = true;
+        noctalia.enable = true;
+        spicetify.enable = true;
+        obs.enable = true;
+      };
+
+      tui = {
+        tmux.enable = true;
+        nvf.enable = true;
+        direnv.enable = true;
+        fish.enable = true;
+      };
+
+      misc = {
+        gaming.enable = true;
+        git.enable = true;
+      };
+    };
+
+    services = {
+      pipewire.enable = true;
+      bluetooth.enable = true;
+      polkit.enable = true;
+      mime.enable = true;
+      security.enable = true;
+    };
+
+    system = {
+      hardware = {
+        nvidia.enable = true;
+        printing.enable = true;
+      };
+
+      networking.enable = true;
+    };
+    wms = {
+      niri.enable = true;
+    };
+  };
+  networking.firewall = {
+    allowedUDPPorts = [10999]; # 10999 for don't starve together
+    allowedTCPPorts = [10999];
+  };
+
+  environment.variables = {
+    PROTONPATH = "GE-Proton";
+    NIXPKGS_ALLOW_UNFREE = 1;
+    GAMEID = "0";
+  };
+  environment.shellAliases = {
+    os-rebuild = "sudo nixos-rebuild switch --flake /home/amr/nixos#Lap";
+    os-rebuild-boot = "sudo nixos-rebuild boot --flake /home/amr/nixos#Lap";
+    grep = "grep --color=auto";
+  };
+
+  services.flatpak.enable = true;
+
+  services.hardware.openrgb = {
+    enable = true;
+    #package = pkgs.openrgb-hardwaresync;
+    motherboard = "amd";
+  };
+  programs.labwc.enable = true;
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      initial_session = {
+        command = "niri-session";
+        user = "amr";
+      };
+      default_session = initial_session;
+    };
+  };
+  system.stateVersion = "24.05";
+}
---
+++ hosts/Lap/packages.nix
@@ -0,0 +1,77 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  pkgs-stable = import inputs.nixpkgs-stable {
+    system = pkgs.system;
+    config.allowUnfree = true;
+  };
+in {
+  environment.systemPackages = [
+    pkgs.ghostty
+    pkgs.zapzap # Whatsapp client
+    pkgs.galculator # Calculator
+    pkgs.ungoogled-chromium
+    pkgs.bitwarden-desktop
+    pkgs.zed-editor
+    pkgs.zed-discord-presence
+    pkgs.playerctl
+    pkgs.gthumb
+    pkgs.zoxide
+    pkgs.inkscape
+    #pkgs.audacity fails to build
+
+    pkgs.pkg-config
+    pkgs.tree
+    pkgs.wget
+    pkgs.git
+    pkgs.gh
+
+    pkgs.unrar
+    pkgs.mission-center
+    pkgs.godot
+
+    (pkgs.blender.override {
+      cudaSupport = true;
+      waylandSupport = true;
+    })
+
+    pkgs.gimp3
+    pkgs.nemo-with-extensions # file manager
+    pkgs.pavucontrol
+    #pkgs.qbittorrent
+    pkgs.kdePackages.ark
+
+    pkgs.dust
+    (pkgs.btop.override {cudaSupport = true;})
+    pkgs.bat
+    pkgs.eza
+    pkgs.libreoffice
+    pkgs.simple-scan
+
+    pkgs.gparted
+    pkgs.lzip
+
+    pkgs.microfetch
+    pkgs.obsidian
+    pkgs.vlc
+    pkgs.udiskie
+    pkgs.gvfs
+    pkgs.udisks
+    pkgs.usbutils
+  ];
+  fonts.packages = with pkgs; [
+    wineWow64Packages.fonts
+    corefonts
+    vista-fonts
+    unifont
+    cascadia-code
+    fira-code
+    inter
+    roboto-serif
+    noto-fonts-color-emoji
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+  ];
+}
