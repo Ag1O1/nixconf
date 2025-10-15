bf3775f02794519347a1c4420404087506c4b592
ag1O1 <amrashraf20210@gmail.com>
2025-10-15 21:16:30 +0300

    comment out filesystem

--- hosts/Lap/hardware/fs.nix
+++ hosts/Lap/hardware/fs.nix
@@ -3,10 +3,11 @@
   services.udisks2.enable = true;
   services.devmon.enable = true;
 
-  fileSystems."/run/media/amr/Disk" = {
-    device = "/dev/disk/by-uuid/d2f9345e-a3be-42c4-a695-1218fee27862";
-    fsType = "ext4";
-  };
+  # TODO
+  #fileSystems."/run/media/amr/Disk" = {
+  #  device = "/dev/disk/by-uuid/d2f9345e-a3be-42c4-a695-1218fee27862";
+  #  fsType = "ext4";
+  #};
 
   # encrypted device
   boot.initrd.luks.devices."luks-40f7a02e-be74-48d1-b150-a3aef34c9b5e".device =
