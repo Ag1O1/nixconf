fa7cd3fc6b6d4630fc51bd9fb01b4dff3ca5cc81
ag1O1 <amrashraf20210@gmail.com>
2025-10-17 10:10:43 +0300

    lap: comment out luks device

--- hosts/Lap/hardware/fs.nix
+++ hosts/Lap/hardware/fs.nix
@@ -12,8 +12,8 @@
   # encrypted device
   boot.initrd.luks.devices."luks-40f7a02e-be74-48d1-b150-a3aef34c9b5e".device =
     "/dev/disk/by-uuid/40f7a02e-be74-48d1-b150-a3aef34c9b5e";
-  boot.initrd.luks.devices."luks-d2f9345e-a3be-42c4-a695-1218fee27862".device =
-    "/dev/disk/by-uuid/d2f9345e-a3be-42c4-a695-1218fee27862";
+  #boot.initrd.luks.devices."luks-d2f9345e-a3be-42c4-a695-1218fee27862".device =
+  #  "/dev/disk/by-uuid/d2f9345e-a3be-42c4-a695-1218fee27862";
 
   ## for ntfs (windows) drives
   #boot.supportedFilesystems = [ "ntfs" ];
