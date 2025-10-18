c9ea090a3dc29a37e7600a30fa79f9802b803e5c
ag1O1 <amrashraf20210@gmail.com>
2025-10-18 14:07:50 +0300

    lap hardware: add asusd

--- hosts/Lap/hardware/fs.nix
+++ hosts/Lap/hardware/fs.nix
@@ -2,6 +2,7 @@
   services.gvfs.enable = true;
   services.udisks2.enable = true;
   services.devmon.enable = true;
+  services.asusd.enable = true;
 
   # TODO
   #fileSystems."/run/media/amr/Disk" = {
