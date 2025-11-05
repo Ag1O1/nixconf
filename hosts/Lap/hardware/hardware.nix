8946b692f0ffd15f62a0f46891c6f84645a7998f
ag1O1 <amrashraf20210@gmail.com>
2025-11-05 18:41:00 +0200

    Laptop: set charge threshold

--- hosts/Lap/hardware/hardware.nix
+++ hosts/Lap/hardware/hardware.nix
@@ -18,6 +18,8 @@
       CPU_BOOST_ON_AC = 1;
       PLATFORM_PROFILE_ON_AC = "performance";
       USB_AUTOSUSPEND = 0;
+      START_CHARGE_THRESH_BAT1= 75;
+      STOP_CHARGE_THRESH_BAT1= 80;
     };
   };
   services.power-profiles-daemon.enable = lib.mkForce false;
