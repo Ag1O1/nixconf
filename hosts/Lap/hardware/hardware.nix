f7747f7333fce972b1d93472e61e94b4f59ba693
ag1O1 <amrashraf20210@gmail.com>
2025-11-22 19:28:05 +0200

    laptop hardware: power tweaks

--- hosts/Lap/hardware/hardware.nix
+++ hosts/Lap/hardware/hardware.nix
@@ -1,36 +1,59 @@
 { lib, ... }:
 {
+  services.logind.settings.Login.HandleLidSwitch = "ignore";
   services.tlp = {
     enable = true;
     settings = {
       NMI_WATCHDOG = 0; # Set to 1 if debugging
+
       CPU_DRIVER_OPMODE_ON_AC = "active";
       CPU_DRIVER_OPMODE_ON_BAT = "active";
+
       RADEON_DPM_STATE_ON_AC = "performance";
       RADEON_DPM_STATE_ON_BAT = "battery";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_BOOST_ON_BAT = 0;
-      PLATFORM_PROFILE_ON_BAT = "balanced";
+
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
       CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
       PLATFORM_PROFILE_ON_AC = "performance";
-      USB_AUTOSUSPEND = 0;
-      START_CHARGE_THRESH_BAT1= 75;
-      STOP_CHARGE_THRESH_BAT1= 80;
+      PLATFORM_PROFILE_ON_BAT = "balanced";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      RUNTIME_PM_ON_AC = "on";
+      RUNTIME_PM_ON_BAT = "auto";
+
+      PCIE_ASPM_ON_AC = "default";
+      PCIE_ASPM_ON_BAT = "powersupersave";
+
+      USB_AUTOSUSPEND = 1;
+
+      START_CHARGE_THRESH_BAT1 = 85;
+      STOP_CHARGE_THRESH_BAT1 = 90;
     };
   };
   services.power-profiles-daemon.enable = lib.mkForce false;
   powerManagement.powertop.enable = true;
-  services.supergfxd.enable = true;
+  services.supergfxd = {
+    enable = true;
+    settings = {
+      #mode = "Integrated";
+      enableOffloading = true;
+      enablePowerManagement = true;
+    };
+  };
   services.xserver.videoDrivers = [
     "amdgpu"
     "nvidia"
   ];
   hardware.nvidia = {
     open = true;
-    powerManagement.finegrained = false;
+    powerManagement.enable = true;
+    powerManagement.finegrained = true;
     prime = {
       offload.enable = true;
       offload.enableOffloadCmd = true;
