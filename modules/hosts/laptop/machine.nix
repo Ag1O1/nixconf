b22b327aaf2b0b0251e2dd1c7b01ff8eabd1579b
ag101 <amrashraf20210@gmail.com>
2026-03-12 13:57:05 +0200

    Update machine.nix

--- modules/hosts/laptop/machine.nix
+++ modules/hosts/laptop/machine.nix
@@ -2,10 +2,18 @@
   flake.nixosConfigurations.laptop = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
+      self.nixosModules.core
+      self.nixosModules.laptopModule
       self.nixosModules.testModule
+      self.nixosModules.niri
+
     ];
   };
-  flake.nixosModules.testModule = {
-    system.stateVersion = "24.11";
+  flake.nixosModules.laptopModule = { pkgs, ... }: {
+    system.stateVersion = "25.11";
+    environment.systemPackages = with pkgs; [
+      godot
+      equibop
+    ];
   };
 }
