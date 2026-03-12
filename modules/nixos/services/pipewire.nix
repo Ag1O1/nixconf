cdd9a6fcf5e18a884297bd75f8e606918d3a30a1
ag101 <amrashraf20210@gmail.com>
2026-03-12 16:51:57 +0200

    Wrap pipewire in flake.nixosModule

--- modules/nixos/services/pipewire.nix
+++ modules/nixos/services/pipewire.nix
@@ -1,24 +1,26 @@
 {...}: {
-  security.rtkit.enable = true;
-  services.pipewire = {
-    alsa.enable = true;
-    enable = true;
-    audio.enable = true;
-    extraConfig.pipewire."92-low-latency" = {
-      "context.properties" = {
-        "default.clock.rate" = 48000;
-        "default.clock.allowed-rates" = [48000];
-        "default.clock.quantum" = 2048;
-        "default.clock.min-quantum" = 2048;
-        "default.clock.max-quantum" = 8192;
-      };
-    };
-    wireplumber = {
+  flake.nixosModules.pipewire = {
+    security.rtkit.enable = true;
+    services.pipewire = {
+      alsa.enable = true;
       enable = true;
-      extraConfig = {
-        "10-disable-camera" = {
-          "wireplumber.profiles" = {
-            main."monitor.libcamera" = "disabled";
+      audio.enable = true;
+      extraConfig.pipewire."92-low-latency" = {
+        "context.properties" = {
+          "default.clock.rate" = 48000;
+          "default.clock.allowed-rates" = [48000];
+          "default.clock.quantum" = 2048;
+          "default.clock.min-quantum" = 2048;
+          "default.clock.max-quantum" = 8192;
+        };
+      };
+      wireplumber = {
+        enable = true;
+        extraConfig = {
+          "10-disable-camera" = {
+            "wireplumber.profiles" = {
+              main."monitor.libcamera" = "disabled";
+            };
           };
         };
       };
