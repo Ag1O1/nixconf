46edff32b288c6724f143d49091891320b9798d6
ag101 <amrashraf20210@gmail.com>
2026-03-13 15:07:06 +0200

    noctalia: fix inputs

--- modules/nixos/programs/noctalia.nix
+++ modules/nixos/programs/noctalia.nix
@@ -1,9 +1,5 @@
-{
-  flake.modules.nixos.noctalia = {
-    inputs,
-    pkgs,
-    ...
-  }: {
+{inputs, ...}: {
+  flake.modules.nixos.noctalia = {pkgs, ...}: {
     services.gnome.evolution-data-server.enable = true;
     environment.systemPackages = with pkgs; [
       (inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default.override {calendarSupport = true;})
