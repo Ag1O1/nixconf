a7bff302362aeafab6bdb4417b309a08e79dbecc
ag101 <amrashraf20210@gmail.com>
2026-03-13 12:07:44 +0200

    Services: add networking

---
+++ modules/nixos/services/networking.nix
@@ -0,0 +1,13 @@
+{...}: {
+  flake.modules.nixos.networking = {
+    networking = {
+      networkmanager = {
+        enable = true;
+      };
+      firewall = {
+        enable = true;
+        #allowedTCPPorts = [];
+      };
+    };
+  };
+}
