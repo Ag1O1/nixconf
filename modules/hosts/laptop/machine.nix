17b4f14ba91ce9d56c84167f12145822b2e06abb
ag101 <amrashraf20210@gmail.com>
2026-03-13 08:57:34 +0200

    laptop: add filesystem

--- modules/hosts/laptop/machine.nix
+++ modules/hosts/laptop/machine.nix
@@ -3,7 +3,7 @@
   self,
   ...
 }: {
-  flake.nixosConfigurations.laptop = inputs.nixpkgs.lib.nixosSystem {
+  flake.nixosConfigurations.laptop = inputs.nixkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
       self.nixosModules.core
@@ -43,13 +43,59 @@
       amdgpuBusId = "PCI:102:0:0";
       nvidiaBusId = "PCI:01:0:0";
     };
-    # Temp fake hardware for testing
+
     fileSystems."/" = {
-      device = "/dev/disk/by-label/nixos";
-      fsType = "ext4";
+      device = "UUID=430c366d-f6d8-4592-a26a-561a29d94de1";
+      fsType = "btrfs";
+      options = [
+        "subvol=@nixos"
+        "compress=zstd:1"
+        "noatime"
+        "discard=async"
+        "autodefrag"
+      ];
     };
 
-    boot.loader.grub.enable = true;
-    boot.loader.grub.device = "nodev";
+    fileSystems."/home" = {
+      device = "UUID=430c366d-f6d8-4592-a26a-561a29d94de1";
+      fsType = "btrfs";
+      options = [
+        "subvol=@home"
+        "compress=zstd:1"
+        "noatime"
+        "discard=async"
+        "autodefrag"
+      ];
+    };
+
+    fileSystems."/nix" = {
+      device = "UUID=430c366d-f6d8-4592-a26a-561a29d94de1";
+      fsType = "btrfs";
+      options = [
+        "subvol=@nix"
+        "compress=zstd:1"
+        "noatime"
+        "discard=async"
+      ];
+    };
+
+    fileSystems."/mnt/swap" = {
+      device = "UUID=430c366d-f6d8-4592-a26a-561a29d94de1";
+      fsType = "btrfs";
+      options = [
+        "subvol=@swap"
+        "noatime"
+      ];
+    };
+
+    swapDevices = [
+      {device = "/mnt/swap/swapfile";}
+    ];
+
+    fileSystems."/home/amr/drive" = {
+      device = "UUID=b75ce50d-1020-4784-824a-dae35069d641";
+      fsType = "ext4";
+      options = ["defaults" "noatime"];
+    };
   };
 }
