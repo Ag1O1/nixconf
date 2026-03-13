edc4955fab8931c2bd1ef1eb67acf03f18020f22
ag101 <amrashraf20210@gmail.com>
2026-03-13 08:48:07 +0200

    Update nvidia.nix

--- modules/nixos/hardware/nvidia.nix
+++ modules/nixos/hardware/nvidia.nix
@@ -1,20 +1,19 @@
-{
-  lib,
-  config,
-  ...
-}:
-with lib; {
+{...}: {
   flake.modules.nixos.nvidia = {
-    services.xserver.videoDrivers = ["nvidia"];
-    hardware = {
-      graphics.enable = true;
-      nvidia = {
-        enable = true;
-        modesetting.enable = true;
-        open = mkDefault true;
-        powerManagement.finegrained = mkDefault false;
-        package = mkDefault config.boot.kernelPackages.nvidiaPackages.beta;
+    config,
+    lib,
+    ...
+  }:
+    with lib; {
+      services.xserver.videoDrivers = ["nvidia"];
+      hardware = {
+        graphics.enable = true;
+        nvidia = {
+          modesetting.enable = true;
+          open = mkDefault true;
+          powerManagement.finegrained = mkDefault false;
+          package = mkDefault config.boot.kernelPackages.nvidiaPackages.beta;
+        };
       };
     };
-  };
 }
