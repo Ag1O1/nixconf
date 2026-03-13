4d92e92c6aeb1d887716875cd906cbeaf0ab1cfc
ag101 <amrashraf20210@gmail.com>
2026-03-13 17:30:53 +0200

    Create theming.nix

---
+++ modules/nixos/theming.nix
@@ -0,0 +1,19 @@
+{
+  flake.modules.nixos.theming = {pkgs, ...}: {
+    environment.systempackages = [
+      pkgs.adw-gtk3
+      pkgs.dracula-icon-theme
+      pkgs.papirus-icon-theme
+      pkgs.bibata-cursors
+      pkgs.xsettingsd
+      pkgs.xorg.xrdb
+    ];
+    environment.sessionVariables = {
+      GTK_THEME = "adw-gtk3";
+    };
+    qt = {
+      enable = true;
+      platformTheme = "qt5ct";
+    };
+  };
+}
