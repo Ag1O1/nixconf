753d1065dc2203d94dfe28d2e88e6aae5bb746ae
ag101 <amrashraf20210@gmail.com>
2026-03-13 14:35:16 +0200

    nix: turn on experimental features

--- modules/nixos/core.nix
+++ modules/nixos/core.nix
@@ -1,6 +1,7 @@
 {...}: {
   flake.modules.nixos.core = {pkgs, ...}: {
     nixpkgs.config.allowUnfree = true;
+    nix.settings.experimental-features = ["nix-command" "flakes"];
     environment.systemPackages = with pkgs; [
       microfetch
       git
