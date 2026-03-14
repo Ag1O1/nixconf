28e18ada3d2dc10ef84dbc60267b57f8e5d298a8
ag101 <amrashraf20210@gmail.com>
2026-03-14 10:29:28 +0200

    noctalia: add satty

--- modules/nixos/programs/noctalia.nix
+++ modules/nixos/programs/noctalia.nix
@@ -5,6 +5,7 @@
     environment.systemPackages = with pkgs; [
       (inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default.override {calendarSupport = true;})
       wlsunset
+      satty
       app2unit
       glib
       adw-gtk3
