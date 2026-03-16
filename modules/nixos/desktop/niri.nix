9c9b1395e2e483f50170dcea0e36ffcabf257a9e
ag101 <amrashraf20210@gmail.com>
2026-03-16 09:09:50 +0200

    niri: update pkgs.system to pkgs.stdenv.hostPlatform.system

--- modules/nixos/desktop/niri.nix
+++ modules/nixos/desktop/niri.nix
@@ -2,7 +2,7 @@
   flake.modules.nixos.niri = {pkgs, ...}: {
     programs.niri = {
       enable = true;
-      package = inputs.linuxmobile-pkgs.packages.${pkgs.system}.niri;
+      package = inputs.linuxmobile-pkgs.packages.${pkgs.stdenv.hostPlatform.system}.niri;
     };
     environment.systemPackages = [pkgs.xwayland-satellite];
   };
