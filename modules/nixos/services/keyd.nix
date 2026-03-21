3da686b93ebc960598d4b1c996647d34d3226e9a
ag101 <amrashraf20210@gmail.com>
2026-03-21 07:13:06 +0200

    keyd: format

--- modules/nixos/services/keyd.nix
+++ modules/nixos/services/keyd.nix
@@ -3,34 +3,34 @@
     services.keyd = {
       enable = true;
       keyboards.default = {
-        ids = [ "*" ];
+        ids = ["*"];
         settings = {
           main = {
-                     capslock = "overload(nav, esc)";
-                     "\\" = "backspace";
-                     backspace = "\\";
-                   };
-                   "shift:S" = {
-                     capslock = "capslock";
-                   };
-                   "nav:C" = {
-                     h = "left";
-                     j = "down";
-                     k = "up";
-                     l = "right";
-                     b = "C-left";
-                     w = "C-right";
-                     "0" = "home";
-                     "4" = "end";
-                     d = "layer(nav_delete)";
-                     i = "pageup";
-                     u = "pagedown";
-                   };
-                   nav_delete = {
-                     w = "C-delete";
-                     b = "C-backspace"; # Standardized for "word" delete
-                   };
-                 };
+            capslock = "overload(nav, esc)";
+            "\\" = "backspace";
+            backspace = "\\";
+          };
+          "shift:S" = {
+            capslock = "capslock";
+          };
+          "nav:C" = {
+            h = "left";
+            j = "down";
+            k = "up";
+            l = "right";
+            b = "C-left";
+            w = "C-right";
+            "0" = "home";
+            "4" = "end";
+            d = "layer(nav_delete)";
+            i = "pageup";
+            u = "pagedown";
+          };
+          nav_delete = {
+            w = "C-delete";
+            b = "C-backspace"; # Standardized for "word" delete
+          };
+        };
       };
     };
   };
