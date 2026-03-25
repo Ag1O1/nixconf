4034e8f3c613a7cc4a3ca5bf78fd6849faff4365
ag101 <amrashraf20210@gmail.com>
2026-03-25 11:28:43 +0200

    niri: explicitly add gnome and gtk portals

--- modules/nixos/desktop/niri.nix
+++ modules/nixos/desktop/niri.nix
@@ -5,5 +5,13 @@
       package = inputs.linuxmobile-pkgs.packages.${pkgs.stdenv.hostPlatform.system}.niri;
     };
     environment.systemPackages = [pkgs.xwayland-satellite pkgs.wlr-which-key pkgs.wooz];
+    xdg.portal = {
+      enable = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gnome
+        pkgs.xdg-desktop-portal-gtk
+      ];
+      config.common.default = ["gnome"];
+    };
   };
 }
