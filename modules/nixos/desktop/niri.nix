6d9d80c652765aff82ebefcb5762906d8139f6a7
ag101 <amrashraf20210@gmail.com>
2026-06-10 14:09:36 +0300

    niri: switch back to normal package

--- modules/nixos/desktop/niri.nix
+++ modules/nixos/desktop/niri.nix
@@ -2,7 +2,6 @@
   flake.modules.nixos.niri = {pkgs, ...}: {
     programs.niri = {
       enable = true;
-      package = inputs.linuxmobile-pkgs.packages.${pkgs.stdenv.hostPlatform.system}.niri;
     };
     environment.systemPackages = [pkgs.xwayland-satellite pkgs.wlr-which-key pkgs.hyprmagnifier];
     xdg.portal = {
