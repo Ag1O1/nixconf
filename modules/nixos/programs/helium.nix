630e2d740861ab879be44a13aac31517dc337e6e
ag101 <amrashraf20210@gmail.com>
2026-07-12 08:13:07 +0300

    fix mistake in helium module

--- modules/nixos/programs/helium.nix
+++ modules/nixos/programs/helium.nix
@@ -1,23 +1,23 @@
 {inputs, ...}: {
   flake.modules.nixos.helium = {
     imports = [inputs.helium-flake.nixosModules.default];
-  };
-  programs.helium = {
-    enable = true;
+    programs.helium = {
+      enable = true;
 
-    flags = [
-      "--disable-gpu"
-      "--ozone-platform-hint=auto"
-    ];
+      flags = [
+        "--disable-gpu"
+        "--ozone-platform-hint=auto"
+      ];
 
-    policies = {
-      "BrowserSignin" = 0;
-      "PasswordManagerEnabled" = false;
-      "SyncDisabled" = true;
-      "DefaultSearchProviderEnabled" = true;
-      "DefaultSearchProviderSearchURL" = "http://localhost:8081/search?q={searchTerms}";
-      "SpellcheckEnabled" = true;
-      "SpellcheckLanguage" = ["en-US"];
+      policies = {
+        "BrowserSignin" = 0;
+        "PasswordManagerEnabled" = false;
+        "SyncDisabled" = true;
+        "DefaultSearchProviderEnabled" = true;
+        "DefaultSearchProviderSearchURL" = "http://localhost:8081/search?q={searchTerms}";
+        "SpellcheckEnabled" = true;
+        "SpellcheckLanguage" = ["en-US"];
+      };
     };
   };
 }
