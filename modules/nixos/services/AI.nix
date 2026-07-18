feb26e4197e399a26ac94a58b68ad01df4f96c63
ag101 <amrashraf20210@gmail.com>
2026-07-18 16:55:27 +0300

    AI: use stablepkgs for openwebui

--- modules/nixos/services/AI.nix
+++ modules/nixos/services/AI.nix
@@ -1,12 +1,24 @@
 {
-  flake.modules.nixos.AI = {pkgs, ...}: {
+  inputs,
+  system,
+  ...
+}: {
+  flake.modules.nixos.AI = {pkgs, ...}: let
+    pkgsStable = import inputs.nixpkgs-stable {
+      system = pkgs.stdenv.hostPlatform.system;
+      config.allowUnfree = true;
+    };
+  in {
     services = {
       ollama = {
         enable = true;
         openFirewall = true;
         package = pkgs.ollama-cuda;
       };
-      open-webui.enable = true;
+      open-webui = {
+        enable = true;
+        package = pkgsStable.open-webui;
+      };
     };
   };
 }
