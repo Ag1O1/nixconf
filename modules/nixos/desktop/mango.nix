6ed0b82cfe3dd6b360644316ccdaec87770a254b
ag101 <amrashraf20210@gmail.com>
2026-08-26 13:15:17 +0300

    mango configuration

--- modules/nixos/desktop/mango.nix
+++ modules/nixos/desktop/mango.nix
@@ -1,216 +1,243 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}: {
   flake.modules = {
-    nixos.mango = {
+    nixos.mango = {pkgs, ...}: {
       programs.mango.enable = true;
       hj.imports = [
         inputs.self.modules.hjem.mango
       ];
+      xdg.portal = {
+        enable = true;
+        wlr.enable = true;
+        wlr.settings = {
+          screencast = {
+            chooser_type = "dmenu";
+            chooser_cmd = "noctalia dmenu";
+          };
+        };
+        extraPortals = [
+          pkgs.xdg-desktop-portal-gtk
+          pkgs.xdg-desktop-portal-wlr
+        ];
+        config.mango = {
+          "org.freedesktop.impl.portal.ScreenCast" = "wlr";
+          "org.freedesktop.impl.portal.Screenshot" = "wlr";
+        };
+      };
     };
     hjem.mango = {
       lib,
       pkgs,
       ...
     }: let
+      mkScript = name: pkgs.writeShellScript name (builtins.readFile ./scripts/${name});
+      fileNames = builtins.attrNames (builtins.readDir ./scripts);
+      scripts = builtins.listToAttrs (map (name: {
+          name = lib.removeSuffix ".sh" name;
+          value = mkScript name;
+        })
+        fileNames);
     in {
-      xdg.config.files."mango/config.conf".text = ''
-        #############################
-        # General
-        #############################
+      xdg.config.files."mango/config.conf".text =
+        #toml
+        ''
+          ### General ###
 
-        default_layout=scroller
-        circle_layout=scroller,tile,monocle,grid
+          circle_layout=scroller,dwindle,grid
 
-        gappih=5
-        gappiv=5
-        gappoh=5
-        gappov=5
+          borderpx=2
+          border_radius=3
+          gappih=3
+          gappiv=3
+          gappoh=3
+          gappov=3
 
-        sensitivity=0.5
-        sloppyfocus=1
+          sloppyfocus=1
 
-        repeat_rate=30
-        repeat_delay=300
-        xkb_rules_layout=us,eg
+          repeat_rate=30
+          repeat_delay=300
+          xkb_rules_layout=us,eg
 
-        trackpad_natural_scrolling=1
+          trackpad_natural_scrolling=1
 
+          ### Mouse ###
 
-        #############################
-        # Scroller
-        #############################
+          trackpad_accel_profile = 1
+          trackpad_accel_speed = 1
 
-        scroller_focus_center=1
-        scroller_prefer_center=1
-        scroller_default_proportion=0.8
+          mouse_accel_profile = 1
+          mouse_accel_speed = 0.1
 
+          ### Scroller ###
 
-        #############################
-        # Blur
-        #############################
-
-        blur=1
-        blur_optimized=1
-
-        blur_params_num_passes=3
-        blur_params_radius=5
-        blur_params_noise=0.02
-        blur_params_brightness=0.9
-        blur_params_contrast=0.9
-        blur_params_saturation=1.1
+          scroller_focus_center=1
+          scroller_prefer_center=1
+          scroller_default_proportion=0.8
 
 
-        #############################
-        # Autostart
-        #############################
+          ### Blur ###
 
-        exec-once=noctalia
+          blur=1
+          blur_optimized=1
+          blur_layer=0 # Broken with noctalia
 
-
-        #############################
-        # Applications
-        #############################
-
-        bind=SUPER,q,spawn,ghostty
-        bind=SUPER,p,spawn,/home/amr/.config/noctalia/hooks/performance-on.sh
-        bind=SUPER+SHIFT,p,spawn,/home/amr/.config/noctalia/hooks/performance-off.sh
-
-        bind=SUPER+SHIFT,c,killclient
-
-        bind=SUPER,s,spawn,wlr-which-key
-        bind=SUPER,w,spawn,helium
-        bind=SUPER,e,spawn,yazi
-        bind=SUPER,a,spawn,${lib.getExe pkgs.hyprmag} -r 2000
+          blur_params_num_passes=3
+          blur_params_radius=5
+          blur_params_noise=0.02
+          blur_params_brightness=0.9
+          blur_params_contrast=0.9
+          blur_params_saturation=1.1
 
 
-        #############################
-        # Noctalia
-        #############################
+          ### Autostart ###
 
-        bind=SUPER,g,spawn,noctalia msg panel-toggle control-center
-        bind=SUPER,r,spawn,noctalia msg panel-toggle launcher
-        bind=SUPER+CTRL,r,spawn,noctalia msg panel-toggle control-center
-        bind=SUPER+SHIFT,r,spawn,noctalia msg panel-toggle clipboard
+          exec-once=noctalia
 
 
-        #############################
-        # Screenshots
-        #############################
+          ### Environment ###
 
-        bind=SUPER+SHIFT,s,spawn_shell,${lib.getExe pkgs.grim} -g "$(${lib.getExe pkgs.slurp})" - | ${lib.getExe' pkgs.wl-clipboard "wl-copy"} --type image/png && notify-send "Screenshot copied to clipboard!"
+          env=PROTONPATH,GE-Proton
+          env=SSH_AUTH_SOCK,/home/amr/.bitwarden-ssh-agent.sock
+          env=ELECTRON_OZONE_PLATFORM_HINT,auto
+          env=QT_QPA_PLATFORMTHEME,qt6ct
+          env=QT_QPA_PLATFORM,wayland
+          env=QT_WAYLAND_DISABLE_WINDOWDECORATION,1
 
-        bind=SUPER+SHIFT+CTRL,s,spawn_shell,${lib.getExe' pkgs.wl-clipboard "wl-paste"} | ${lib.getExe pkgs.satty} --filename -
+          ### Applications ###
 
-        bind=SUPER+SHIFT+ALT,s,spawn_shell,${lib.getExe pkgs.grim} -g "$(${lib.getExe pkgs.slurp} -o)" - | ${lib.getExe' pkgs.wl-clipboard "wl-copy"} --type image/png && notify-send "Screenshoted window copied to clipboard!"
+          bind=SUPER,q,spawn,ghostty
+          bind=SUPER,t,spawn,ghostty -e ${scripts.todo}
+          bind=SUPER,p,spawn,/home/amr/.config/noctalia/hooks/performance-on.sh
+          bind=SUPER+SHIFT,p,spawn,/home/amr/.config/noctalia/hooks/performance-off.sh
 
+          bind=SUPER+SHIFT,c,killclient
 
-        #############################
-        # XF86
-        #############################
-
-        bind=NONE,XF86AudioRaiseVolume,spawn,noctalia msg volume-up
-        bind=NONE,XF86AudioLowerVolume,spawn,noctalia msg volume-down
-        bind=NONE,XF86AudioMute,spawn,noctalia msg volume-mute
-        bind=NONE,XF86AudioMicMute,spawn,noctalia msg mic-mute
-
-        bind=NONE,XF86AudioNext,spawn,noctalia msg media next
-        bind=NONE,XF86AudioPause,spawn,noctalia msg media toggle
-        bind=NONE,XF86AudioPlay,spawn,noctalia msg media toggle
-        bind=NONE,XF86AudioPrev,spawn,noctalia msg media previous
-
-        bind=NONE,XF86MonBrightnessUp,spawn,noctalia msg brightness-up
-        bind=NONE,XF86MonBrightnessDown,spawn,noctalia msg brightness-down
+          bind=SUPER,s,spawn,wlr-which-key
+          bind=SUPER,w,spawn,helium
+          bind=SUPER,e,spawn,ghostty -e yazi
+          bind=SUPER,a,spawn,${lib.getExe pkgs.hyprmag} -r 2500
 
 
-        #############################
-        # Tags
-        #############################
+          ### Noctalia ###
 
-        bind=SUPER,1,view,1
-        bind=SUPER,2,view,2
-        bind=SUPER,3,view,3
-        bind=SUPER,4,view,4
-        bind=SUPER,5,view,5
-        bind=SUPER,6,view,6
-        bind=SUPER,7,view,7
-        bind=SUPER,8,view,8
-        bind=SUPER,9,view,9
-
-        bind=SUPER+SHIFT,1,tag,1
-        bind=SUPER+SHIFT,2,tag,2
-        bind=SUPER+SHIFT,3,tag,3
-        bind=SUPER+SHIFT,4,tag,4
-        bind=SUPER+SHIFT,5,tag,5
-        bind=SUPER+SHIFT,6,tag,6
-        bind=SUPER+SHIFT,7,tag,7
-        bind=SUPER+SHIFT,8,tag,8
-        bind=SUPER+SHIFT,9,tag,9
+          bind=SUPER,g,spawn,noctalia msg panel-toggle control-center
+          bind=SUPER,r,spawn,noctalia msg panel-toggle launcher
+          bind=SUPER+CTRL,r,spawn,noctalia msg panel-toggle control-center
+          bind=SUPER+SHIFT,r,spawn,noctalia msg panel-toggle clipboard
 
 
-        #############################
-        # Adjacent Tags
-        #############################
+          ### Screenshots ###
 
-        bind=SUPER,d,viewtoleft
-        bind=SUPER,c,viewtoright
+          bind=SUPER+SHIFT,s,spawn_shell,${lib.getExe pkgs.grim} -g "$(${lib.getExe pkgs.slurp})" - | ${lib.getExe' pkgs.wl-clipboard "wl-copy"} --type image/png && notify-send "Screenshot copied to clipboard!"
 
+          bind=SUPER+SHIFT+CTRL,s,spawn_shell,${lib.getExe' pkgs.wl-clipboard "wl-paste"} | ${lib.getExe pkgs.satty} --filename -
 
-        #############################
-        # Focus
-        #############################
-
-        bind=SUPER,z,focusdir,left
-        bind=SUPER,x,focusdir,right
+          bind=SUPER+SHIFT+ALT,s,spawn_shell,${lib.getExe pkgs.grim} -g "$(${lib.getExe pkgs.slurp} -o)" - | ${lib.getExe' pkgs.wl-clipboard "wl-copy"} --type image/png && notify-send "Screenshoted window copied to clipboard!"
 
 
-        #############################
-        # Layouts
-        #############################
+          ### XF86 ###
 
-        # Cycle layouts
-        bind=SUPER+SHIFT,TAB,switch_layout
+          bind=NONE,XF86AudioRaiseVolume,spawn,noctalia msg volume-up
+          bind=NONE,XF86AudioLowerVolume,spawn,noctalia msg volume-down
+          bind=NONE,XF86AudioMute,spawn,noctalia msg volume-mute
+          bind=NONE,XF86AudioMicMute,spawn,noctalia msg mic-mute
 
-        # Explicit layouts
-        bind=SUPER+CTRL,1,setlayout,scroller
-        bind=SUPER+CTRL,2,setlayout,tile
-        bind=SUPER+CTRL,3,setlayout,monocle
-        bind=SUPER+CTRL,4,setlayout,grid
+          bind=NONE,XF86AudioNext,spawn,noctalia msg media next
+          bind=NONE,XF86AudioPause,spawn,noctalia msg media toggle
+          bind=NONE,XF86AudioPlay,spawn,noctalia msg media toggle
+          bind=NONE,XF86AudioPrev,spawn,noctalia msg media previous
 
-
-        #############################
-        # Scroller
-        #############################
-
-        bind=SUPER+ALT,e,set_proportion,1.0
-        bind=SUPER+ALT,x,switch_proportion_preset
+          bind=NONE,XF86MonBrightnessUp,spawn,noctalia msg brightness-up
+          bind=NONE,XF86MonBrightnessDown,spawn,noctalia msg brightness-down
 
 
-        #############################
-        # Window States
-        #############################
+          ### Tags ###
 
-        bind=SUPER,f,togglemaximizescreen
-        bind=SUPER+ALT,f,togglefullscreen
-        bind=SUPER+SHIFT,f,togglefullscreen
+          tagrule=id:1,layout_name:scroller
+          tagrule=id:2,layout_name:scroller
+          tagrule=id:3,layout_name:scroller
+          tagrule=id:4,layout_name:scroller
+          tagrule=id:5,layout_name:scroller
+          tagrule=id:6,layout_name:scroller
+          tagrule=id:7,layout_name:scroller
+          tagrule=id:8,layout_name:scroller
+          tagrule=id:9,layout_name:scroller
 
-        bind=SUPER,v,togglefloating
+          bind=SUPER,1,comboview,1
+          bind=SUPER,2,comboview,2
+          bind=SUPER,3,comboview,3
+          bind=SUPER,4,comboview,4
+          bind=SUPER,5,comboview,5
+          bind=SUPER,6,comboview,6
+          bind=SUPER,7,comboview,7
+          bind=SUPER,8,comboview,8
+          bind=SUPER,9,comboview,9
+
+          bind=SUPER+SHIFT,1,tag,1
+          bind=SUPER+SHIFT,2,tag,2
+          bind=SUPER+SHIFT,3,tag,3
+          bind=SUPER+SHIFT,4,tag,4
+          bind=SUPER+SHIFT,5,tag,5
+          bind=SUPER+SHIFT,6,tag,6
+          bind=SUPER+SHIFT,7,tag,7
+          bind=SUPER+SHIFT,8,tag,8
+          bind=SUPER+SHIFT,9,tag,9
+
+          bind=SUPER+CTRL+SHIFT,1,toggletag,1
+          bind=SUPER+CTRL+SHIFT,2,toggletag,2
+          bind=SUPER+CTRL+SHIFT,3,toggletag,3
+          bind=SUPER+CTRL+SHIFT,4,toggletag,4
+          bind=SUPER+CTRL+SHIFT,5,toggletag,5
+          bind=SUPER+CTRL+SHIFT,6,toggletag,6
+          bind=SUPER+CTRL+SHIFT,7,toggletag,7
+          bind=SUPER+CTRL+SHIFT,8,toggletag,8
+          bind=SUPER+CTRL+SHIFT,9,toggletag,9
+
+          ### Focus ###
+
+          mousebind=SUPER,btn_left,moveresize,curmove
+          mousebind=SUPER,btn_right,moveresize,curresize
+          bind=SUPER,z,focusdir,left
+          bind=SUPER,x,focusdir,right
+          bind=SUPER,d,focusdir,up
+          bind=SUPER,c,focusdir,down
+          bind=SUPER,Tab,focusstack,next
+          bind=SUPER+SHIFT,Tab,focusstack,prev
 
 
-        #############################
-        # Overview
-        #############################
+          ### Layouts ###
 
-        bind=SUPER,TAB,toggleoverview
+          # Cycle layouts
+          bind=SUPER+SHIFT,TAB,switch_layout
+
+          # Explicit layouts
+          bind=SUPER+CTRL,1,setlayout,scroller
+          bind=SUPER+CTRL,2,setlayout,dwindle
+          bind=SUPER+CTRL,3,setlayout,grid
+          bind=SUPER+CTRL,4,setlayout,monocle
+
+          ### Window States ###
+
+          bind=SUPER,f,togglemaximizescreen
+          bind=SUPER+SHIFT,f,togglefullscreen
+
+          bind=SUPER,v,togglefloating
 
 
-        #############################
-        # Config / Session
-        #############################
+          ### Config / Session ###
 
-        bind=SUPER+SHIFT,o,reload_config
-        bind=SUPER+SHIFT,DELETE,quit
+          bind=SUPER+SHIFT,o,reload_config
+          bind=SUPER+SHIFT,DELETE,quit
 
-        bind=SUPER,space,switch_keyboard_layout
-      '';
+          bind=SUPER,space,switch_keyboard_layout
+
+          ### sources ###
+          source=~/.config/mango/noctalia.conf
+        '';
     };
   };
 }
