11354f3cdebcc6e52336d1abbff73208954d6a65
ag101 <amrashraf20210@gmail.com>
2026-09-02 09:27:52 +0300

    ly: import pkgs properly and add missing services. prefix

--- modules/nixos/programs/ly.nix
+++ modules/nixos/programs/ly.nix
@@ -1,11 +1,11 @@
-{pkgs, ...}: {
-  flake.modules.nixos.ly = let
+{
+  flake.modules.nixos.ly = {pkgs, ...}: let
     animation-dur = pkgs.fetchurl {
-      url = "https://codeberg.org/fairyglade/ly-community/src/branch/main/animations/dur/blackhole-smooth-240x67.dur";
+      url = "https://codeberg.org/fairyglade/ly-community/raw/branch/main/animations/dur/blackhole-smooth-240x67.dur";
       hash = "";
     };
   in {
-    displayManager.ly = {
+    services.displayManager.ly = {
       enable = true;
       settings = {
         animation = "dur_file";
