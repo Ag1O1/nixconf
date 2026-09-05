a424b842a630adcead1e79cb89a81da7d1264e5c
ag101 <amrashraf20210@gmail.com>
2026-09-05 09:26:31 +0300

    mime: add hjem setting

--- modules/nixos/services/mime/mime.nix
+++ modules/nixos/services/mime/mime.nix
@@ -2,7 +2,11 @@
   # Deconflict the namespace by pulling flake out of it
   inherit (config) flake;
 in {
-  flake.modules.nixos.mime = {config, ...}: let
+  flake.modules.nixos.mime = {
+    config,
+    lib,
+    ...
+  }: let
     inherit
       # These are nixos modules options and not a member of flake
       (config.modules.services.mime)
@@ -16,30 +20,35 @@ in {
       zip
       file-manager
       ;
+
+    defaultApplications = {
+      "text/*" = "${text}.desktop";
+      "inode/directory" = "${file-manager}";
+
+      "image/*" = "${image}.desktop";
+      "video/*" = "${video}.desktop";
+      "audio/*" = "${audio}.desktop";
+
+      "x-scheme-handler/http" = "${browser}.desktop";
+      "x-scheme-handler/https" = "${browser}.desktop";
+
+      "application/zip" = "${zip}.desktop";
+      "application/x-rar-compressed" = "${zip}.desktop";
+      "application/x-7z-compressed" = "${zip}.desktop";
+      "application/pdf" = "${pdf}.desktop";
+      "application/x-blender" = "blender.desktop";
+      "application/x-godot-project" = "org.godotengine.Godot4.desktop";
+      "image/svg+xml" = "${svg}.desktop";
+    };
   in {
     # This cames from the flake parts module
     imports = [flake.modules.nixos.mimeOptions];
     xdg.mime = {
       enable = true;
-      defaultApplications = {
-        "text/*" = "${text}.desktop";
-        "inode/directory" = "${file-manager}";
-
-        "image/*" = "${image}.desktop";
-        "video/*" = "${video}.desktop";
-        "audio/*" = "${audio}.desktop";
-
-        "x-scheme-handler/http" = "${browser}.desktop";
-        "x-scheme-handler/https" = "${browser}.desktop";
-
-        "application/zip" = "${zip}.desktop";
-        "application/x-rar-compressed" = "${zip}.desktop";
-        "application/x-7z-compressed" = "${zip}.desktop";
-        "application/pdf" = "${pdf}.desktop";
-        "application/x-blender" = "blender.desktop";
-        "application/x-godot-project" = "org.godotengine.Godot4.desktop";
-        "image/svg+xml" = "${svg}.desktop";
-      };
+      inherit defaultApplications;
+    };
+    hj.files.".config/mimeapps.list".text = lib.generators.toINI {} {
+      "Default Applications" = defaultApplications;
     };
   };
 }
